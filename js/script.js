dc6949e1c69680b9e2703c01458ce8fddb5c3a57
yyhago <yhago.felipe.teles@gmail.com>
2025-08-06 02:09:56 -0300

    feat: search input functionally!

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const userName = document.querySelector(".userName");
+const avatar_url = document.querySelector("#avatar_url");
+const userBio = document.querySelector(".userBio");
 const statNumberRepos = document.querySelector("#statNumberRepos");
 const statNumberSeguidores = document.querySelector("#statNumberSeguidores");
 const statNumberSeguindo = document.querySelector("#statNumberSeguindo");
@@ -7,24 +9,34 @@ const url_API = "https://api.github.com/users";
 let dadosAPI = null;
 
 
+
 async function consultarDadosAPI(usuario){
     try {
-        const response = await fetch(`${url_API}/${usuario}`)
-        const json = await response.json()
+        const response = await fetch(`${url_API}/${usuario}`);
+        const json = await response.json();
 
-        dadosAPI = json
-        coletarDados(dadosAPI)
+        dadosAPI = json;
+        coletarDados(dadosAPI);
 
     } catch (error) {
-        console.log("Erro na requisição: ", error)
+        console.log("Erro na requisição: ", error);
     }
 }
 
-async function coletarDados(dados){
-    userName.innerHTML = dados.name
-    statNumberRepos.innerHTML = dados.public_repos
-    statNumberSeguidores.innerHTML = dados.followers
-    statNumberSeguindo.innerHTML = dados.following
+
+async function pesquisarUsuario(event){
+    event.preventDefault();
+
+    let usuario = document.querySelector(".inputSearch").value;
+    consultarDadosAPI(usuario)
 }
 
-consultarDadosAPI("yyhago")
+
+async function coletarDados(dados){
+    userName.innerHTML = dados.name;
+    statNumberRepos.innerHTML = dados.public_repos;
+    statNumberSeguidores.innerHTML = dados.followers;
+    statNumberSeguindo.innerHTML = dados.following;
+    userBio.innerHTML = dados.bio;
+    avatar_url.src = dados.avatar_url;
+}
