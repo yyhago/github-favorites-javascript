c6fdb1fc187c9e4065c995f8b32dd46f38dd7cca
yyhago <yhago.felipe.teles@gmail.com>
2025-08-06 00:36:39 -0300

    feat: connection with api github

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const userName = document.querySelector(".userName");
+const statNumberRepos = document.querySelector("#statNumberRepos");
+const statNumberSeguidores = document.querySelector("#statNumberSeguidores");
+const statNumberSeguindo = document.querySelector("#statNumberSeguindo");
+
+const url_API = "https://api.github.com/users";
+let dadosAPI = null;
+
+
+async function consultarDadosAPI(usuario){
+    try {
+        const response = await fetch(`${url_API}/${usuario}`)
+        const json = await response.json()
+
+        dadosAPI = json
+        coletarDados(dadosAPI)
+
+    } catch (error) {
+        console.log("Erro na requisição: ", error)
+    }
+}
+
+async function coletarDados(dados){
+    userName.innerHTML = dados.name
+    statNumberRepos.innerHTML = dados.public_repos
+    statNumberSeguidores.innerHTML = dados.followers
+    statNumberSeguindo.innerHTML = dados.following
+}
+
+consultarDadosAPI("yyhago")
